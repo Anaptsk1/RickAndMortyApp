4b35c29b79d68c35fec3105c61ed78c308bf06b1
Ana Ptskialadze <anka@Anas-MacBook-Pro.local>
2024-05-04 19:14:42 +0400

    adding swiftUI contentView to implement the app in swiftUI.

---
+++ RickAndMortyApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  RickAndMortyApp
+//
+//  Created by Ana Ptskialadze on 04.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
